540cb2e41cbd632db957d6a5d71ff66a27ed02b2
nepolx <vasilyevaapolina2006@gmail.com>
2025-10-03 15:00:19 +0300

    [TASK-2-1] correct lint2

--- polina.vasileva/task-2-1/cmd/service/main.go
+++ polina.vasileva/task-2-1/cmd/service/main.go
@@ -14,39 +14,56 @@ type ConditionerT struct {
 func main() {
 	var departNum int
 	_, err := fmt.Scan(&departNum)
+
 	if err != nil {
 		fmt.Println("Invalid input")
+
 		return
 	}
+
 	for range departNum {
 		var emplCount int
 		_, err := fmt.Scan(&emplCount)
+
 		if err != nil {
 			fmt.Println("Invalid input")
+
 			return
 		}
+
 		conditioner := ConditionerT{MINTEMP, MAXTEMP, true}
+
 		for range emplCount {
 			var sign string
-			var degrees int
 			_, err = fmt.Scan(&sign)
+
 			if err != nil {
 				fmt.Println("Invalid input")
+
 				return
 			}
+
+			var degrees int
 			_, err = fmt.Scan(&degrees)
+
 			if err != nil {
 				fmt.Println("Invalid input")
+
 				return
 			}
+
+			if !conditioner.status {
+				continue
+			}
+
+			changeTemp(&conditioner, sign, degrees)
+
 			if conditioner.status {
-				changeTemp(&conditioner, sign, degrees)
-				if conditioner.status {
-					fmt.Println(conditioner.minTemp)
-				} else {
-					fmt.Println("-1")
-					return
-				}
+				fmt.Println(conditioner.minTemp)
+			} else {
+				fmt.Println("-1")
+
+				return
 			}
 		}
 	}
@@ -55,30 +72,38 @@ func main() {
 func changeTemp(cond *ConditionerT, sign string, degrees int) {
 	switch sign {
 	case ">=":
-		if degrees >= cond.minTemp {
-			if degrees <= cond.maxTemp {
-				cond.minTemp = degrees
-			} else {
-				if degrees < cond.minTemp {
-					cond.status = true
-				} else {
-					cond.status = false
-				}
-			}
-		}
+		handleGreaterEqual(cond, degrees)
 	case "<=":
-		if degrees <= cond.maxTemp {
-			if degrees >= cond.minTemp {
-				cond.maxTemp = degrees
-			} else {
-				if degrees > cond.maxTemp {
-					cond.status = true
-				} else {
-					cond.status = false
-				}
-			}
-		}
+		handleLessEqual(cond, degrees)
 	default:
 		cond.status = false
 	}
+}
+
+func handleGreaterEqual(cond *ConditionerT, degrees int) {
+	if degrees < cond.minTemp {
+		return
+	}
+
+	if degrees > cond.maxTemp {
+		cond.status = false
+
+		return
+	}
+
+	cond.minTemp = degrees
+}
+
+func handleLessEqual(cond *ConditionerT, degrees int) {
+	if degrees > cond.maxTemp {
+		return
+	}
+
+	if degrees < cond.minTemp {
+		cond.status = false
+
+		return
+	}
+
+	cond.maxTemp = degrees
 }
