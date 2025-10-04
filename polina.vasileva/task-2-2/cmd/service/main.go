639ff6b3d200986af3730d8d272f710f1d6d652d
nepolx <vasilyevaapolina2006@gmail.com>
2025-10-04 23:40:15 +0300

    [TASK-2-2] fix lint

--- polina.vasileva/task-2-2/cmd/service/main.go
+++ polina.vasileva/task-2-2/cmd/service/main.go
@@ -6,22 +6,26 @@ import (
 )
 
 func main() {
-	var dishNum, k int
+	var (
+		dishNum, k int
+	)
 
 	_, err := fmt.Scan(&dishNum)
 	if err != nil {
 		fmt.Println("Invalid input", err)
+
 		return
 	}
 
 	h := &IntHeap{}
 	heap.Init(h)
 
-	for i := 0; i < dishNum; i++ {
+	for range dishNum {
 		var temp int
 		_, err := fmt.Scan(&temp)
 		if err != nil {
 			fmt.Println("Invalid input", err)
+
 			return
 		}
 		heap.Push(h, temp)
@@ -30,6 +34,7 @@ func main() {
 	_, err = fmt.Scan(&k)
 	if err != nil {
 		fmt.Println("Invalid input", err)
+
 		return
 	}
 
@@ -58,5 +63,6 @@ func (h *IntHeap) Pop() any {
 	n := len(old)
 	x := old[n-1]
 	*h = old[0 : n-1]
+
 	return x
 }
