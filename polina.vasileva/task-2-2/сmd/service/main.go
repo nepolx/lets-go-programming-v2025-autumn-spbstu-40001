7cc28672b31305ce1756cb41473ad12f595bd9c7
nepolx <vasilyevaapolina2006@gmail.com>
2025-10-04 23:11:06 +0300

    [TASK-2-2] real initial commit

---
+++ polina.vasileva/task-2-2/сmd/service/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"container/heap"
+	"fmt"
+	//"github.com/nepolx/task-2-1/pkg/IntHeap"
+)
+
+func main() {
+	var (
+		dishNum, k int
+	)
+
+	_, err := fmt.Scan(&dishNum)
+	if err != nil {
+		fmt.Println("Invalid input", err)
+
+		return
+	}
+
+	h := &IntHeap{}
+	heap.Init(h)
+
+	for i := 0; i < dishNum; i++ {
+    	var temp int
+    	_, err := fmt.Scan(&temp)
+    	if err != nil {
+        	fmt.Println("Invalid input", err)
+
+        	return
+    	}
+    	heap.Push(h, temp)
+	}
+
+	_, err = fmt.Scan(&k)
+	if err != nil {
+		fmt.Println("Invalid input", err)
+
+		return
+	}
+
+	for range k - 1 {
+		if h.Len() == 0 {
+			fmt.Println("There is no such dish")
+		}
+		heap.Pop(h)
+	}
+
+	fmt.Println(heap.Pop(h))
+
+}
+
+type IntHeap []int
+
+func (h *IntHeap) Len() int           { return len(*h) }
+func (h *IntHeap) Less(i, j int) bool { return (*h)[i] > (*h)[j] }
+func (h *IntHeap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
+
+func (h *IntHeap) Push(x any) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+
+	return x
+}
