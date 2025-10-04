b67740f02c325bfda85ddc14f2fa9925b32ffb86
nepolx <vasilyevaapolina2006@gmail.com>
2025-10-05 00:04:40 +0300

    [TASK-2-2] add pkg

--- polina.vasileva/task-2-2/cmd/service/main.go
+++ polina.vasileva/task-2-2/cmd/service/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"container/heap"
 	"fmt"
+	"github.com/nepolx/task-2-2/pkg/intheap"
 )
 
 func main() {
@@ -49,28 +50,4 @@ func main() {
 	}
 
 	fmt.Println(heap.Pop(intheap))
-}
-
-type IntHeap []int
-
-func (h *IntHeap) Len() int           { return len(*h) }
-func (h *IntHeap) Less(i, j int) bool { return (*h)[i] > (*h)[j] }
-func (h *IntHeap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
-
-func (h *IntHeap) Push(x interface{}) {
-	value, ok := x.(int)
-	if !ok {
-		return
-	}
-
-	*h = append(*h, value)
-}
-
-func (h *IntHeap) Pop() any {
-	old := *h
-	n := len(old)
-	x := old[n-1]
-	*h = old[0 : n-1]
-
-	return x
 }
---
+++ polina.vasileva/task-2-2/pkg/intheap/internal.go
@@ -0,0 +1,25 @@
+package intheap
+
+type IntHeap []int
+
+func (h *IntHeap) Len() int           { return len(*h) }
+func (h *IntHeap) Less(i, j int) bool { return (*h)[i] > (*h)[j] }
+func (h *IntHeap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
+
+func (h *IntHeap) Push(x interface{}) {
+	value, ok := x.(int)
+	if !ok {
+		return
+	}
+
+	*h = append(*h, value)
+}
+
+func (h *IntHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+
+	return x
+}
