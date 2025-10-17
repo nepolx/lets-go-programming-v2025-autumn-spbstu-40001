ea1a4d0b868a3191b93dcfc40e8d86641dbde25d
nepolx <vasilyevaapolina2006@gmail.com>
2025-10-17 16:47:45 +0300

    [TASK-3] lint

--- polina.vasileva/task-3/internal/json/parsejson.go
+++ polina.vasileva/task-3/internal/json/parsejson.go
@@ -15,11 +15,11 @@ func ParseJSON[T any](filePath string, data T) error {
 
 	directory := filepath.Dir(filePath)
 
-	if err := os.MkdirAll(directory, 0755); err != nil {
+	if err := os.MkdirAll(directory, 0o755); err != nil {
 		return fmt.Errorf("cannot create directory '%s': %w", directory, err)
 	}
 
-	if err := os.WriteFile(filePath, jsonData, 0600); err != nil {
+	if err := os.WriteFile(filePath, jsonData, 0o600); err != nil {
 		return fmt.Errorf("cannot write to file '%s': %w", filePath, err)
 	}
 
