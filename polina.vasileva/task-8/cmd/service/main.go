34a76fbb9807c5f46309949b664019f46e393cf3
nepolx <vasilyevaapolina2006@gmail.com>
2025-12-22 18:40:16 +0300

    [TASK-8] add config

--- polina.vasileva/task-8/cmd/service/main.go
+++ polina.vasileva/task-8/cmd/service/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/nepolx/task-8/internal/config"
+)
+
+func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%s %s", cfg.Environment, cfg.LogLevel)
+}
